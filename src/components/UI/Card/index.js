166833b49137d003032358c2eed074b132070362
danieltadresu <danieldev1999@gmail.com>
2021-07-24 15:40:05 -0400

    chore: add CRA and Basic Layout

---
+++ src/components/UI/Card/index.js
@@ -0,0 +1,97 @@
+import profile from "../../../images/profile.png";
+
+const Card = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              {/* <img src={profile} className="h-7 sm:h-8 rounded-lg rounded-full"/> */}
+            </div>
+            <div className="pt-6 text-base leading-6 font-semibold sm:text-lg sm:leading-7 tracking-tighter text-gray-800">
+              <p>Hello, I'm Daniel 👋</p>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p>
+                  Web Developer passionate in producing real solutions using the
+                  best available tools.
+                </p>
+                <ul className="list-disc space-y-2">
+                  <li className="flex items-start">
+                    <span className="h-6 flex items-center sm:h-7">
+                      <svg
+                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    <p className="ml-2">Web Apps</p>
+                  </li>
+                  <li className="flex items-start">
+                    <span className="h-6 flex items-center sm:h-7">
+                      <svg
+                        class="flex-shrink-0 h-5 w-5 text-cyan-500"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    <p className="ml-2">APIS</p>
+                  </li>
+                  <li className="flex items-start">
+                    <span className="h-6 flex items-center sm:h-7">
+                      <svg
+                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    <p className="ml-2">Mobile Apps</p>
+                  </li>
+                </ul>
+                <p>
+                  Interested in state of the art technology. Focused in high
+                  quality code and top-tier development/deployment procedures.
+                </p>
+              </div>
+              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                <p>Let's build something together!</p>
+                <p>
+                  <a
+                    href="https://tailwindcss.com/docs"
+                    className="text-cyan-600 hover:text-cyan-700"
+                  >
+                    {" "}
+                    Contact &rarr;{" "}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
